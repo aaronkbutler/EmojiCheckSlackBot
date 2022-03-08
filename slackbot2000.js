737a64a1463b34ee33a4adc239fe3427f5bbfd16
Aaron Butler <aaronbutler@wustl.edu>
2022-03-08 06:23:16 -0500

    finished slackbot2000

---
+++ slackbot2000.js
@@ -0,0 +1,54 @@
+const { App } = require("@slack/bolt");
+
+const EmojiTranslate = require("./emoji.js");
+
+require("dotenv").config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+const twilioNumber = process.env.TWILIO_NUMBER;
+
+const channelIds = { goodVibez: process.env.GOOD_VIBEZ, badVibez: process.env.BAD_VIBEZ, botResponse: process.env.BOT_RESPONSE, testBot: process.env.TEST_BOT };
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode:true, // enable the following to use socket mode
+  appToken: process.env.APP_TOKEN
+});
+
+async function sendMessages(phoneNumbers, jsonData) {
+  for (const phoneNumber of phoneNumbers) {
+    const message = client.messages.create({
+      body: JSON.stringify(jsonData, ["type", "score", "ratio", "keywords", "word", "result_code", "result_msg"], 4),
+      from: twilioNumber,
+      to: phoneNumber
+    });
+  }
+}
+
+app.message(async ({ message, say }) => {
+  //console.log(message);
+  if(message.channel !== channelIds.badVibez) {
+      for (const emoji of EmojiTranslate.translate(message.text, true)) {
+          console.log("Emoji: " + emoji + ".");
+	  if (emoji !== '' && emoji.length > 1) {
+              try {
+                await app.client.reactions.add({ token: process.env.SLACK_BOT_TOKEN, channel: message.channel, name: emoji.toLowerCase(), timestamp: message.ts});
+              } catch (error) {
+                  console.log(error);
+              }
+          }
+      }
+  }
+});
+
+(async () => {
+  const port = 5001
+  // Start your app
+  await app.start(process.env.PORT || port);
+  console.log(`⚡ Slack Bolt app is running on port ${port}!`);
+})();
